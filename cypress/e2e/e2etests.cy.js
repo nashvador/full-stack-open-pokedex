40ca1f7e4d85373c19a0570f63e519e93233356d
nashvador <nashvador@gmail.com>
2022-09-21 11:31:38 -0400

    Fix end to end tests to speed up workflow

--- cypress/e2e/e2etests.cy.js
+++ cypress/e2e/e2etests.cy.js
@@ -1,15 +1,15 @@
 describe("Pokedex", function () {
   it("front page can be opened", function () {
-    cy.visit("http://localhost:5000"); // eslint-disable-line no-use-before-define
-    cy.contains("ivysaur"); // eslint-disable-line no-use-before-define
-    // eslint-disable-next-line no-use-before-define
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur");
     cy.contains(
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
 
   it("pokemon page can be navigated to", function () {
-    cy.visit("http://localhost:5000/pokemon/ivysaur"); // eslint-disable-line no-use-before-define
-    cy.contains("overgrow"); // eslint-disable-line no-use-before-define
+    cy.visit("http://localhost:5000/");
+    cy.contains("ivysaur").click();
+    cy.contains("overgrow");
   });
 });
